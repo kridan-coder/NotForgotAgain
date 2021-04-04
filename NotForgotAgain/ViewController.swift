be498d00ab5a16fd30e9732b5cd97d345a34839f
Daniil <krivelevdaniil@gmail.com>
2021-04-05 02:18:48 +0700

    Initial Commit

---
+++ NotForgotAgain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NotForgotAgain
+//
+//  Created by Daniil Zavodchanovich on 05.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
