b9a8060de49ad43feaa27038dbc22aea707e3521
Daniil <krivelevdaniil@gmail.com>
2021-04-15 13:40:40 +0700

    splash screen set, font added, project structure set

---
+++ NotForgotAgain/Scenes/Coordinator.swift
@@ -0,0 +1,8 @@
+//
+//  Coordinator.swift
+//  NotForgotAgain
+//
+//  Created by KriDan on 15.04.2021.
+//
+
+import Foundation
---
+++ NotForgotAgain/Scenes/Login/LoginCoordinator.swift
@@ -0,0 +1,8 @@
+//
+//  LoginCoordinator.swift
+//  NotForgotAgain
+//
+//  Created by KriDan on 15.04.2021.
+//
+
+import Foundation
