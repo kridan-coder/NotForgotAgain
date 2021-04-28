d66d74200e5cb88c0986e99b810989dabbd50eae
Daniil <krivelevdaniil@gmail.com>
2021-04-28 14:26:29 +0700

    Added image set for checkbox (checked and unchecked). Implemented logic for checkbox (which is unfortunately not provided by Xcode) by creating special class "CheckBox". Handed down all extensions in separate file. Created xib files and VC's for CellTask (The colored one with header and brief description) and CellCategory (The one with word above the colored tasks). Arranged elements inside xibs -> layouts for them are done

---
+++ NotForgotAgain/Scenes/Check Tasks/ViewControllers/CellCategoryViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCellCategory.swift
+//  NotForgotAgain
+//
+//  Created by KriDan on 23.04.2021.
+//
+
+import UIKit
+
+class CellCategoryViewController: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
